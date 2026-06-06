595ad2decb8e73d0bfbf074f7365f4c11e868c9a
zeads <ruri.pelinandang@gmail.com>
2026-06-06 20:52:44 +0700

    feat: Create model and migration Order, OrderItem

---
+++ database/migrations/2026_06_06_134620_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('order_number')->unique();
+            $table->string('status')->default('pending');
+            $table->decimal('subtotal', 15, 2);
+            $table->decimal('shipping_cost', 15, 2);
+            $table->decimal('grand_total', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
