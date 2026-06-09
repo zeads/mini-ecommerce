47d89a531836d3fcfa5ea9950195042dc867f190
zeads <ruri.pelinandang@gmail.com>
2026-06-09 11:31:47 +0700

    feat: Create Factory Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->unique()->words(3, true);
+        return [
+            'category_id' => Category::factory(),
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'description' => fake()->paragraph(),
+            'price' => fake()->numberBetween(
+                100000,
+                5000000
+            ),
+            'stock' => fake()->numberBetween(
+                10,
+                200
+            ),
+            'image' => null,
+        ];
+    }
+}
