cd983001d18057437ce95f12f6c3d46b26d07857
zeads <ruri.pelinandang@gmail.com>
2026-06-10 17:25:12 +0700

    feat: Create Product Seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Category extends Model
 {
+    use HasFactory;
+
     public function products()
     {
         return $this->hasMany(Product::class);
